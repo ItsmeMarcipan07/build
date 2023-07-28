2d7d7874bd36e3b8a1f6434580db6e21854d0abf
ItsmeMarcipan07 <martipan07@abv.bg>
2023-07-28 11:43:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import sys
+import pathlib
+
 # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, 'C:\\Users\SESA732254\PycharmProjects\pythonProject1\\build\exe.win-amd64-3.10')
+sys.path.insert(1, f"{pathlib.Path().resolve()}")
 from data_from_user import data
 
 # getting information from data_from_user.py
@@ -10,3 +12,5 @@ PLC_PORT = int(data[1])  # get PLC PORT
 PLC_NAME = str(data[2])  # get PLC NAME
 UNIT = data[3]  # get UNIT
 API_KEY = str(data[4])  # get API KEY
+Latitude = float(data[5])  # get latitude
+Longitude = float(data[6])  # get logitude
