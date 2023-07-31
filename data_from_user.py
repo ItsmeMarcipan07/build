4ecf88af091594f8d8f20cc67b58d272dd3f0dc9
ItsmeMarcipan07 <martipan07@abv.bg>
2023-07-31 11:25:33 +0300

    Update data_from_user.py

--- data_from_user.py
+++ data_from_user.py
@@ -3,8 +3,11 @@ import pathlib
 
 # caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.insert(1, f"{pathlib.Path().resolve()}")
+
 # getting settings from settings.txt
-data = []  # empty var
+
+data = []  # empty var/list
+
 with open("settings.txt") as MyFile:  # open settings file
     for line in MyFile:  # loop
         data.append(line.split()[-1])  # getting information
