dee156b2287cda0aa5bea58f3bc11bcdc86e29e4
ItsmeMarcipan07 <martipan07@abv.bg>
2023-08-03 11:03:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,15 @@
 import sys
 import pathlib
 
-# caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.insert(1, f"{pathlib.Path().resolve()}")
-# imports
 from data_from_user import data
 
 # getting information from data_from_user.py
 
-PLC_IP = str(data[0])  # get PLC IP
-PLC_PORT = int(data[1])  # get PLC PORT
-PLC_NAME = str(data[2])  # get PLC NAME
-UNIT = data[3]  # get UNIT
-API_KEY = str(data[4])  # get API KEY
-Latitude = float(data[5])  # get latitude
-Longitude = float(data[6])  # get logitude
+PLC_IP: str = str(data[0])  # get PLC IP
+PLC_PORT: int = int(data[1])  # get PLC PORT
+PLC_NAME: str = str(data[2])  # get PLC NAME
+UNIT: int = data[3]  # get UNIT
+API_KEY: str = str(data[4])  # get API KEY
+Latitude: float = float(data[5])  # get latitude
+Longitude: float = float(data[6])  # get logitude
